6b04699bddde2d32e758678867571313499af3e6
evgeny <polevikovea@gmail.com>
2023-05-05 07:30:10 +0300

    Update consumer, add script sample

--- consumer/src/main/java/org/epolevikov/kafkaconsumer/Consumer.java
+++ consumer/src/main/java/org/epolevikov/kafkaconsumer/Consumer.java
@@ -3,15 +3,21 @@ package org.epolevikov.kafkaconsumer;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 @Component
 public class Consumer {
-  private final Logger logger = LoggerFactory.getLogger(Consumer.class);
 
   @KafkaListener(topics = "messages", id = "group_id")
-  public void listen(String message) {
-    logger.info(message);
+  public void listen(String message) throws IOException {
+    message += "\n";
+
+    Files.write(
+      Paths.get("messages.txt"),
+      message.getBytes(),
+      StandardOpenOption.APPEND);
   }
 }
