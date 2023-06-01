8d1aee88a33323ed72c178eb985eb1dc5a81eee1
evgeny <polevikovea@gmail.com>
2023-06-01 14:40:48 +0300

    Add one more kafka listener

--- consumer/src/main/java/org/epolevikov/kafkaconsumer/Consumer.java
+++ consumer/src/main/java/org/epolevikov/kafkaconsumer/Consumer.java
@@ -11,12 +11,21 @@ import java.nio.file.StandardOpenOption;
 @Component
 public class Consumer {
 
-  @KafkaListener(topics = "messages", id = "group_id")
-  public void listen(String message) throws IOException {
+  @KafkaListener(topics = "messages", id = "consumer_1")
+  public void listen1(String message) throws IOException {
+    writeMessage(message, "messages_1.txt");
+  }
+
+  @KafkaListener(topics = "messages", id = "consumer_2")
+  public void listen2(String message) throws IOException {
+    writeMessage(message, "messages_2.txt");
+  }
+
+  private void writeMessage(String message, String filename) throws IOException {
     message += "\n";
 
     Files.write(
-      Paths.get("messages.txt"),
+      Paths.get(filename),
       message.getBytes(),
       StandardOpenOption.APPEND);
   }
