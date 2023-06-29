62972213faddaacf4cfe8f7de1061fe737c8899b
evgeny <polevikovea@gmail.com>
2023-06-29 14:53:10 +0300

    Update messages producer

--- resources/messages_producer.py
+++ resources/messages_producer.py
@@ -4,12 +4,20 @@ import requests
 def main():
     with open("messages.csv") as messages:
         for i, message in enumerate(messages):
-            if i == 0:
+            if i == 0: # skip header
                 continue
 
             topic, message = message.strip().split(",")
+
+            request_body = {
+                "topic": topic,
+                "message": message
+            }
             
-            requests.post("http://localhost:9000/api/messages")
+            response = requests.post("http://localhost:9000/api/messages", json=request_body)
+            
+            if 200 <= response.status_code < 300:
+                print(f"messaage '{message}' successfully sent to topic '{topic}'")
 
 
 if __name__ == "__main__":
